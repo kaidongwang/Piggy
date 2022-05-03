a643063fc879217b9bbfd91a628885195e8672c9
Kaidong Wang <102991147+kaidongwang@users.noreply.github.com>
2022-05-03 18:42:28 +0000

    dance

--- student.py
+++ student.py
@@ -67,7 +67,6 @@ class Piggy(PiggyParent):
         
         # lower-ordered example...
         self.right(primary=50, counter=50)
-        self.deg_few(90
         self.fwd()
         time.sleep(2)
         self.turn_by_deg(-50)
