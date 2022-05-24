34fb72d3231bc5c5b7ebbb47c035a3bf34832b36
Kaidong Wang <102991147+kaidongwang@users.noreply.github.com>
2022-05-24 19:33:27 +0000

    maze

--- student.py
+++ student.py
@@ -195,7 +195,7 @@ class Piggy(PiggyParent):
             self.stop()
             
             self.fwd()
-            self.sleep(2)
+            time.sleep(2)
             self.stop()
 
             self.left(primary=30, counter=-30)
